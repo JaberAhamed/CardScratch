2df6c785e972b7bbc2fe4ac8d3491ada5f7f0c86
jaber <ahamedjaber8@gmail.com>
2023-08-28 00:28:04 +0600

    :art: change library file dependency

--- Scractch/build.gradle.kts
+++ Scractch/build.gradle.kts
@@ -42,9 +42,7 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.10.1")
-    implementation ("androidx.appcompat:appcompat:1.6.1")
-    implementation ("com.google.android.material:material:1.9.0")
+    implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
     implementation("androidx.activity:activity-compose:1.7.2")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
