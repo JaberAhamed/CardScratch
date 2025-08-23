e502608e5636964c81d7f0ccf8121f6618338cd0
jaber <ahamedjaber8@gmail.com>
2025-08-24 00:00:31 +0600

    updae compile sdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.jaber.scratchcard"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.jaber.scratchcard"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
